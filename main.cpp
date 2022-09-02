d5e729c1a48d8acbf684d90f0040e2b588005eb6
Katrina "Kat" Webb <98193581+Kat-Webb@users.noreply.github.com>
2022-09-01 19:21:51 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -43,6 +43,12 @@ int main() {
 void permutation(int n, int container[], int factorial) {
 
     int perm = (factorial / n);
+    if (n == 1){
+        cout << "1";
+    }
+    else if(n == 2){
+        cout << "{ 1, 2}" << endl << "{ 2, 1}" << endl;
+    }
     //this loop ensures that we pass through each possible number in the array in slot 0
  if(n % 2 == 0){
 
@@ -122,12 +128,16 @@ void permutation(int n, int container[], int factorial) {
     }
 }
 
+
+
 if (n % 2 != 0 && n > 1 ) {
+
+    }
     for (int m = 0; m <= n - 1; m++) {
         //this loop makes sure that we go through each possible number in slot zero as well
         //tried changing perm/2 to perm/3 and that made it so it only printed 3 of the 4 different leading numbers
 
-        for (int j = ((perm + 1) / 2); j > 0; j--) {
+        for (int j = 0; j < (perm + 1) / 2; j++) {
             //for (int i = perm; i >= (perm / 2); i--) {
             int temp = container[1];
             container[1] = container[n - 1];
@@ -146,8 +156,8 @@ if (n % 2 != 0 && n > 1 ) {
             }
 
             //if there are more than two items in the list, swap the middle item with the item after it and keep doing that until we get to the end
-            if (n > 2) {
-                for (int l = 1; l < (n - 2); l++) {
+            if (n > 3) {
+                for (int l = 0; l < (n - 1); l++) {
                     int temp = container[l];
                     container[l] = container[l + 1];
                     container[l + 1] = temp;
@@ -167,7 +177,7 @@ if (n % 2 != 0 && n > 1 ) {
         }
 
         //NO TOUCHY!!!!
-        for (int j = (perm +1 )/ 2; j >= 0; j--) {
+        for (int j = (perm) / 2; j >= 0; j--) {
             //if (j != 0) {
             int temp = container[j];
             container[j] = container[n - 1];
@@ -175,7 +185,8 @@ if (n % 2 != 0 && n > 1 ) {
 
 
             //go ahead and print that since it's a new permutation
-            /*    cout << "{";
+            if (container[0] > j) {
+                cout << "{";
                 for (int k = 0; k <= (n - 1); k++) {
                     if (k != n - 1) {
                         cout << container[k] << ", ";
@@ -183,22 +194,22 @@ if (n % 2 != 0 && n > 1 ) {
                         cout << container[k] << "}" << " LOOP 3 " << endl;
 
                     }
-                }*/
+                }
+            }
         }
 
-    }
+        for (int q = 0; q <= n - 1; q++) {
 
-    for (int q = 0; q <= n - 1; q++) {
+            if (container[q] == (container[0] + 1)) {
+                int temp = container[q];
+                container[q] = container[0];
+                container[0] = temp;
 
-        if (container[q] == (container[0] + 1)) {
-            int temp = container[q];
-            container[q] = container[0];
-            container[0] = temp;
+                break;
+            }
+
         }
-
     }
-
 }
 
 
-}
